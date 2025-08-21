82d7d2576ace333e0d7b15350291bf89eeb6d58b
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-21 18:45:28 +0000

    Jekyll content restored/adapted to SvelteKit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,24 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" />
+  <title>sfmunoz</title>
+  <style>
+    .icon {
+      vertical-align: sub;
+      padding-right: 0.25rem;
+      display: inline-block;
+      width: 1.5em;
+      height: 1.3em;
+      margin-right: 0.2rem;
+      stroke-width: 0;
+      stroke: currentColor;
+      fill: currentColor;
+    }
+    .avatar {
+      border-radius: 10em;
+    }
+  </style>
 </svelte:head>
 
 {@render children?.()}
