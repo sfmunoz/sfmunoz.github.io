65f5d72dc391bc3407e101fe4d29e000031bbc1d
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-22 14:52:35 +0000

    simple.css and related stuff dropped

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,13 @@
 <script lang="ts">
   import "../app.css";
   import favicon from "$lib/assets/favicon.ico";
-  
+
   let { children } = $props();
 </script>
 
 <svelte:head>
   <title>sfmunoz</title>
   <link rel="icon" href={favicon} />
-  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" />
-  <style>
-    .icon {
-      vertical-align: sub;
-      padding-right: 0.25rem;
-      display: inline-block;
-      width: 1.5em;
-      height: 1.3em;
-      margin-right: 0.2rem;
-      stroke-width: 0;
-      stroke: currentColor;
-      fill: currentColor;
-    }
-    .avatar {
-      border-radius: 10em;
-    }
-    body > header {
-      background-color: #d5f7ff;
-    }
-  </style>
 </svelte:head>
 
 {@render children?.()}
