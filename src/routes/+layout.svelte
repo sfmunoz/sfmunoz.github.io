13fab2e281925a20a6fcfa509ae60271c8a43e8a
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-24 15:47:47 +0000

    root skeleton of the app moved from +page.svelte to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import "../app.css";
   import favicon from "$lib/assets/favicon.ico";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import SiteHeader from "$lib/components/site-header.svelte";
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
 
   let { children } = $props();
 </script>
@@ -10,4 +13,14 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="[--header-height:calc(--spacing(14))]">
+  <Sidebar.Provider class="flex flex-col">
+    <SiteHeader />
+    <div class="flex flex-1">
+      <AppSidebar />
+      <Sidebar.Inset>
+        {@render children?.()}
+      </Sidebar.Inset>
+    </div>
+  </Sidebar.Provider>
+</div>
