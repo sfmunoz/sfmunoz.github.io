5c707c9904a07de59132f7a2c8e0e2f76ac98fdf
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-25 19:04:39 +0000

    tips/slug page header dropped

--- src/routes/tips/[slug]/+page.svelte
+++ src/routes/tips/[slug]/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-  import { resolve } from "$app/paths";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 </script>
 
-<ul>
-  <li><a href={resolve("/tips")}>tips</a></li>
-  <li>title: {data.data.title}</li>
-  <li><a href={resolve(`/tips/${data.slug}`)}>/tips/{data.slug}</a></li>
-</ul>
-
-<div class="mx-auto prose dark:prose-invert">{@html data.html}</div>
+<div class="mx-auto mt-8 prose dark:prose-invert">{@html data.html}</div>
