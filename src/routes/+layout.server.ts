2edbc044460d97fef884a2d480a91014c6404e61
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-25 18:25:30 +0000

    tips/+page.server.ts replaced by +layout.server.ts to have widely available info

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
-import type { PageServerLoad } from "./$types";
+import type { LayoutServerLoad } from "./$types";
 import { getTips } from "$lib/getTips";
 
 export const load = (async () => {
   return { tips: getTips() };
-}) satisfies PageServerLoad;
+}) satisfies LayoutServerLoad;
