8087d460bd8d68c9a39abde7620e725fb849b5dd
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-26 15:25:28 +0000

    tips main page style slightly improved

--- src/routes/tips/+page.svelte
+++ src/routes/tips/+page.svelte
@@ -9,10 +9,14 @@
   }));
 </script>
 
-<h1 class="text-dark dark:text-light font-bold text-3xl text-center my-4">
-  Tips
-</h1>
-
-{#each links as link (link.href)}
-  <li><a href={link.href}>{link.name}</a></li>
-{/each}
+<div class="bg-muted/50 rounded-md p-4 flex flex-col gap-5">
+  <h1 class="text-dark dark:text-light font-bold text-3xl text-center mt-8">
+    Tips
+  </h1>
+  <p class="text-center">Choose one of the following tips</p>
+  <ul class="mx-auto list-disc [&>li]:mt-2">
+    {#each links as link (link.href)}
+      <li><a href={link.href}>{link.name}</a></li>
+    {/each}
+  </ul>
+</div>
