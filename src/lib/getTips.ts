c9fa04a57029185a4e6755ac90d370a96e80a9f6
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-30 16:15:26 +0000

    getTips() function dropped (no longer used)

--- src/lib/getTips.ts
+++ src/lib/getTips.ts
@@ -1,10 +1,5 @@
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter";
-
-const re = new RegExp("^[a-z]/[a-z][a-z]/(.+)/index.md$");
-const tipsFolder = path.resolve("src/tips");
-const contentFolder = path.resolve("src/content");
 
 const cmsFolder = (): string => {
   const flist = [
@@ -29,47 +24,9 @@ export const getTipList = (): getTipListRet[] => {
   return js.pages;
 };
 
-export interface getTipsRet {
-  slug: string;
-  data: { [key: string]: any };
-  content: string;
-}
-
 export const getTip = (slug: string): string => {
   const folder = cmsFolder();
   const fname = path.join(folder, "public", "tips", slug, "index.html");
   if (!fs.existsSync(fname)) throw "getTip(): cannot find " + slug;
   return fs.readFileSync(fname, "utf-8");
-};
-
-export const getTips = (): getTipsRet[] => {
-  if (fs.existsSync(contentFolder)) {
-    return fs
-      .readdirSync(contentFolder, { recursive: true })
-      .filter((entry) => {
-        return !!re.exec(entry as string);
-      })
-      .map((entry) => {
-        const m = re.exec(entry as string);
-        if (!m) return { slug: "", data: {}, content: "" }; // never happens because of the previous filter()
-        // m[0] = p/py/python-subprocess/index.md
-        // m[1] = python-subprocess
-        const filePath = path.join(contentFolder, m[0]);
-        const slug = m[1];
-        const buf = fs.readFileSync(filePath, "utf-8");
-        const { data, content } = matter(buf);
-        return { slug, data, content };
-      })
-      .sort((a, b) => a.slug.localeCompare(b.slug));
-  }
-  return fs
-    .readdirSync(tipsFolder)
-    .filter((slug) => fs.existsSync(path.join(tipsFolder, slug, "index.md")))
-    .map((slug) => {
-      const filePath = path.join(tipsFolder, slug, "index.md");
-      const buf = fs.readFileSync(filePath, "utf-8");
-      const { data, content } = matter(buf);
-      return { slug, data, content };
-    })
-    .sort((a, b) => a.slug.localeCompare(b.slug));
 };
