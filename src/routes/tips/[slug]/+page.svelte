c9fe67406df0862bcdfc7bdea1a62f2ae6a81283
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-30 07:21:33 +0000

    tips: individual pages provided with title, summary and date

--- src/routes/tips/[slug]/+page.svelte
+++ src/routes/tips/[slug]/+page.svelte
@@ -4,4 +4,19 @@
   let { data }: PageProps = $props();
 </script>
 
-<div class="mx-auto mt-8 prose dark:prose-invert">{@html data.html}</div>
+<div class="mx-auto mt-8 prose dark:prose-invert">
+  <h1>
+    {#if data.data.title}
+      {data.data.title}
+    {:else}
+      {data.slug}
+    {/if}
+  </h1>
+  {#if data.data.summary}
+    <p class="italic ml-3">
+      {data.data.summary}
+    </p>
+  {/if}
+  {#if data.data.date}<p class="font-light text-sm">{data.data.date}</p>{/if}
+  {@html data.html}
+</div>
