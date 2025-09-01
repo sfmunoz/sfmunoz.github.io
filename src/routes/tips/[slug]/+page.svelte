dba67d48067c63d2d880f603b261ef52849834e1
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-09-01 17:45:52 +0000

    tips: onMount() executes mermaid.run()

--- src/routes/tips/[slug]/+page.svelte
+++ src/routes/tips/[slug]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
+  import mermaid from "mermaid";
+  import { onMount } from "svelte";
 
   let { data }: PageProps = $props();
+
+  // onMount(() => mermaid.run());
+  onMount(() => mermaid.run({ nodes: document.querySelectorAll(".mermaid") }));
 </script>
 
 <div class="mx-auto mt-8 prose dark:prose-invert">
